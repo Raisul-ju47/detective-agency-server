2ec0cf25bb2eb46a411e4012dd926f5a305a9137
Raisul-ju47 <raisulislam698.ri@gmail.com>
2021-04-20 23:43:13 +0600

    final

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 const fileUpload = require("express-fileupload");
+const ObjectId = require("mongodb").ObjectId;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nl16z.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
@@ -38,6 +39,7 @@ client.connect((err) => {
 
   app.post("/addAppointment", (req, res) => {
     const appointment = req.body;
+    console.log(appointment);
     appointmentCollection.insertOne(appointment).then((result) => {
       res.send(result.insertedCount > 0);
     });
@@ -48,6 +50,22 @@ client.connect((err) => {
       res.send(documents);
     });
   });
+  app.get("/appointments", (req, res) => {
+    //console.log(req.query.email);
+    appointmentCollection
+      .find({ email: req.query.email })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+  app.get('/queries', (req, res) => {
+      appointmentCollection.find()
+      .forEach(function(document) {
+          console.log(document.email);
+          res.send(document.email);
+      })
+  })
+
 
   app.post("/appointmentsByDate", (req, res) => {
     const date = req.body;
@@ -84,6 +102,7 @@ client.connect((err) => {
   app.post("/addService", (req, res) => {
     const file = req.files.file;
     const name = req.body.name;
+    const cost = req.body.cost;
     const newImg = file.data;
     const encImg = newImg.toString("base64");
 
@@ -92,7 +111,7 @@ client.connect((err) => {
       size: file.size,
       img: Buffer.from(encImg, "base64"),
     };
-    serviceCollection.insertOne({ name, image }).then((result) => {
+    serviceCollection.insertOne({ name, cost, image }).then((result) => {
       res.send(result.insertedCount > 0);
     });
   });
@@ -105,6 +124,22 @@ client.connect((err) => {
       res.send(result.insertedCount > 0);
     });
   });
+  app.post("/addReview", (req, res) => {
+    const name = req.body.name;
+    const company = req.body.company;
+    const description = req.body.description;
+
+    testimonialCollection
+      .insertOne({ name, company, description })
+      .then((result) => {
+        res.send(result.insertedCount > 0);
+      });
+  });
+  app.get("/reviews", (req, res) => {
+    testimonialCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
   app.get("/detectives", (req, res) => {
     detectiveCollection.find({}).toArray((err, documents) => {
       res.send(documents);
@@ -115,6 +150,21 @@ client.connect((err) => {
       res.send(documents);
     });
   });
+  app.get("/service/:id", (req, res) => {
+    serviceCollection
+      .find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      });
+  });
+  app.delete("/delete/:id", (req, res) => {
+    serviceCollection
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        res.send(result.deletedCount > 0);
+      });
+  });
+  
 
   app.post("/isDetective", (req, res) => {
     const email = req.body.email;
